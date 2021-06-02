c210fc7f419b8ef4d000af5dc8c1e2b1ee58821e
victoriathorsen <75598376+victoriathorsen@users.noreply.github.com>
2021-06-02 19:44:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const createTask = (e) => {
+    e.preventDefault();
+    
+    let list = document.createElement('li');
+    let newTask = e.target['new-task-description'].value;
+    list.innerText = newTask
+    
+    const priority = document.querySelector('#priority')
+    const colorPriority = priority.value
+    if (colorPriority == "Low") {
+      list.style.color = 'green'
+    } else if (colorPriority == "Medium"){
+      list.style.color = 'orange'
+    } else if (colorPriority == "High") {
+      list.style.color = 'red'
+    }
+
+    const button = document.createElement('button')
+    button.innerHTML = "delete"
+    button.addEventListener('click', removeTask);
+    list.appendChild(button)
+
+    document.querySelector('ul').append(list);
+    e.target.reset();
+    colorPriority.reset()
+  };
+
+  const removeTask = () => {
+    let myUl = document.querySelector('ul')
+    let find = document.querySelector('li')
+    myUl.removeChild(find)
+  };
+
+ 
+  document.querySelector('#create-task-form').addEventListener('submit', createTask);
+  
 });
+
+
